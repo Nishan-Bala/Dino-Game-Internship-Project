0989baed253c8002c1c99ec294cca8e3c1d84e16
Nishan-Bala <1balanis@hdsb.ca>
2026-06-09 10:06:42 -0400

    added lots

--- main.py
+++ main.py
@@ -6,13 +6,46 @@ Made by intern: @bassemfarid, no one or nothing else. 🤖
 
 import pygame
 import random
-
+from operator import itemgetter
 # Initialize Pygame and create a window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 running = True  # Pygame main loop, kills pygame when False
+
+score_file = "scores.txt"
+
+def load_scores():
+    scores = []
+    file = open(score_file, "r")
+    for line in file:
+        line = line.strip()
+        if "," in line:
+            name, score = line.rsplit(",", 1)
+            scores.append((name, int(score)))
+            scores.sort(key=itemgetter(1), reverse=True)
+    file.close()
+    return scores
+
+def save_score(name, score):
+    scores = load_scores()
+    scores.append((name, score))
+    scores.sort(key = itemgetter(1), reverse=True)
+
+    file = open(score_file, "w")
+    for name, score in scores:
+        file.write(f"{name},{score}\n")
+    file.close()
+
+def cal_score(start_time, total_time_paused):
+    current_time = pygame.time.get_ticks() - start_time - total_time_paused
+    score = int(current_time / 100)
+    return score
+
+is_entering_name = False
+username = ""
+    
 
 is_settings = False
 sound_effects_vol = 50
@@ -22,6 +55,7 @@ jump_sound.set_volume(sound_effects_vol/100)
 # Game state variables
 small_font = pygame.font.Font(pygame.font.get_default_font(), 28)
 mini_font = pygame.font.Font(pygame.font.get_default_font(), 18)
+current_font_color = "Black"
 is_playing = True  # Whether in game or in menu
 GROUND_Y = 300  # The Y-coordinate of the ground level
 JUMP_GRAVITY_START_SPEED = -15  # The speed at which the player jumps
@@ -34,8 +68,11 @@ time_paused = 0
 total_time_paused = 0
 
 # Load level assets
+is_night = False
 HEART_SURF = pygame.image.load("graphics/level/heart.png").convert_alpha()
 SKY_SURF = pygame.image.load("graphics/level/sky.png").convert()
+SKY_NIGHT_SURF = pygame.image.load("graphics/level/sky_night.png").convert()
+current_sky = SKY_SURF
 GROUND_SURF_1 = pygame.image.load("graphics/level/ground.png").convert()
 GROUND_SURF_2 = pygame.image.load("graphics/level/ground.png").convert()
 ground_rect_1 = GROUND_SURF_1.get_rect(topleft = (800, GROUND_Y))
@@ -61,27 +98,45 @@ SPRING_DURATION = 600
 heart_powerup_surf = pygame.image.load("graphics/power_ups/health_powerup.png").convert_alpha()
 heart_powerup_surf = pygame.transform.scale(heart_powerup_surf, (64, 64))
 heart_rect = heart_powerup_surf.get_rect(bottomleft = (800, GROUND_Y))
-heart_hitbox = heart_rect.inflate(-10,-10)
+heart_hitbox = heart_rect.inflate(-20,-20)
+armour_powerup_surf = pygame.image.load("graphics/power_ups/armour.png").convert_alpha()
+armour_powerup_surf = pygame.transform.scale(armour_powerup_surf, (50, 50))
+armour_rect = armour_powerup_surf.get_rect(bottomleft=(800, GROUND_Y))
+armour_hitbox = armour_rect.inflate(-10, -10)
+armour_active = False
+armour_timer = 0
+ARMOUR_DURATION = 600
+
 
 # Load sprite assets
 player_jump = pygame.image.load("graphics/player/player_jump.png").convert_alpha()
+player_jump_night = pygame.image.load("graphics/player/player_jump_night.png").convert_alpha()
+current_jump = player_jump
 player_walk_1 = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_walk_2 = pygame.image.load("graphics/player/player_walk_2.png").convert_alpha()
+player_walk_1_night = pygame.image.load("graphics/player/player_walk_1_night.png").convert_alpha()
+player_walk_2_night = pygame.image.load("graphics/player/player_walk_2_night.png").convert_alpha()
+player_walk_1_armour = pygame.image.load("graphics/player/player_walk_1_armour.png").convert_alpha()
+player_walk_2_armour = pygame.image.load("graphics/player/player_walk_2_armour.png").convert_alpha()
+player_walk_1_night_armour = pygame.image.load("graphics/player/player_walk_1_night_armour.png").convert_alpha()
+player_walk_2_night_armour = pygame.image.load("graphics/player/player_walk_2_night_armour.png").convert_alpha()
 player_frames = [player_walk_1, player_walk_2] # Animation frame list
 player_frame_index = 0  # Tracks which frame is currently shown
 animation_speed = 8    # How many game frames to wait before switching sprite frame
 animation_counter = 0   # Counts up each game frame to trigger animation switch
 
+
 player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(bottomleft=(25, GROUND_Y))
-player_hitbox = player_rect.inflate(-8, -8)
+player_hitbox = player_rect.inflate(-11, -8)
 egg_rotation_angle = 0
 egg_surf = pygame.image.load("graphics/egg/egg_1.png").convert_alpha()
 egg_rect = egg_surf.get_rect(bottomleft=(800, GROUND_Y))
-egg_hitbox = egg_rect.inflate(-6,-6)
+egg_cracked = pygame.image.load("graphics/egg/egg_cracked.png").convert_alpha()
+egg_hitbox = egg_rect.inflate(-7,-7)
 egg_width = egg_surf.get_width()
 is_double_egg = False
-
+is_cracked = False
 
 lives = 3
 is_invincible = False      
@@ -135,18 +190,54 @@ while running:
                         elif event.key == pygame.K_RIGHT:
                             sound_effects_vol = min(100, sound_effects_vol + 5)
                             jump_sound.set_volume(sound_effects_vol / 100)
+                        elif event.key == pygame.K_n:
+                            is_night = not is_night
+                            if is_night:
+                                    current_sky = SKY_NIGHT_SURF
+                                    current_jump = player_jump_night
+                                    if armour_active:
+                                        player_frames = [player_walk_1_night_armour, player_walk_2_night_armour]
+                                    else:
+                                        player_frames = [player_walk_1_night, player_walk_2_night]
+                                    current_font_color = "White"
+                            else:
+                                current_sky = SKY_SURF
+                                current_jump = player_jump
+                                if armour_active:
+                                    player_frames = [player_walk_1_armour, player_walk_2_armour]
+                                else:
+                                    player_frames = [player_walk_1, player_walk_2]
+                                current_font_color = "Black"
         else:
-            # When player wants to play again by pressing SPACE
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                is_playing = True
-                lives = 3                  
-                is_invincible = False      
-                egg_rect.left = 800
-                start_time = pygame.time.get_ticks()
-                spring_active = False
-                spring_timer = 0
-                time_paused = 0
-                total_time_paused = 0
+            if is_entering_name:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN and username.strip():
+                        save_score(username.strip(), score)
+                        is_entering_name = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        username = username[:-1]
+                    elif event.unicode.isprintable() and len(username) < 15:
+                        username += event.unicode
+            else:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    is_playing = True
+                    lives = 3
+                    is_invincible = False
+                    egg_rect.left = 800
+                    start_time = pygame.time.get_ticks()
+                    spring_active = False
+                    spring_timer = 0
+                    armour_active = False
+                    armour_timer = 0
+                    is_cracked = False
+                    time_paused = 0
+                    total_time_paused = 0
+                    username = ""
+                    # Reset player frames to normal
+                    if is_night:
+                        player_frames = [player_walk_1_night, player_walk_2_night]
+                    else:
+                        player_frames = [player_walk_1, player_walk_2]
 
     if is_playing:
         if not is_paused:
@@ -154,10 +245,18 @@ while running:
                 spring_timer -= 1
                 if spring_timer <= 0:
                     spring_active = False
+            if armour_active:
+                armour_timer -= 1
+                if armour_timer <= 0:
+                    armour_active = False
+                    if is_night:
+                        player_frames = [player_walk_1_night, player_walk_2_night]
+                    else:
+                        player_frames = [player_walk_1, player_walk_2]
             time_paused = 0
             screen.fill("purple")  
             # Wipe the screen# Blit the level assets
-            screen.blit(SKY_SURF, (0, 0))
+            screen.blit(current_sky, (0, 0))
             ground_rect_1.x -= 5
             ground_rect_2.x -= 5
             if ground_rect_1.right < 0:
@@ -175,23 +274,26 @@ while running:
             screen.blit(cloud_1, cloud_1_rect)
             screen.blit(cloud_2, cloud_2_rect)
 
-            current_time = pygame.time.get_ticks() - start_time - total_time_paused
-            score = int(current_time / 100)
-            score_surf = game_font.render(f"Score: {score}", False, "Black")
+            score = cal_score(start_time, total_time_paused)
+            score_surf = game_font.render(f"Score: {score}", False, current_font_color)
             score_rect = score_surf.get_rect(center=(400, 50))
             screen.blit(score_surf, score_rect)
 
-            # Adjust egg's horizontal location then blit it
-            egg_rect.x -= 5
+            egg_speed = 5 + min(score // 150, 10)
+            egg_rect.x -= egg_speed
             if egg_rect.right <= 0:
                 egg_rect.left = 800
-                is_double_egg = random.random() < 0.2  # 20% chance of 2 eggs
-
-            egg_rotation_angle = (egg_rotation_angle + 7) % 360  # negative = clockwise
+                is_double_egg = random.random() < 0.21
+                is_cracked = False
+ 
+            egg_rotation_angle = (egg_rotation_angle + egg_speed * 1.4) % 360
             rotated_egg = pygame.transform.rotate(egg_surf, egg_rotation_angle)
             rotated_rect = rotated_egg.get_rect(center=egg_rect.center)
-            screen.blit(rotated_egg, rotated_rect)
-
+            if is_cracked:
+                screen.blit(egg_cracked, egg_rect)
+            else:
+                screen.blit(rotated_egg, rotated_rect)
+ 
             if is_double_egg:
                 second_center = (egg_rect.centerx + egg_width, egg_rect.centery)
                 screen.blit(rotated_egg, rotated_egg.get_rect(center=second_center))
@@ -215,6 +317,10 @@ while running:
                     if egg_rect.left > 200:
                         active_powerup = 'heart'
                         heart_rect.left = 800
+                elif roll < 0.6:
+                    if egg_rect.left > 200:
+                        active_powerup = 'armour'
+                        armour_rect.left = 800
 
             if active_powerup == 'spring':
                 spring_rect.x -= 5
@@ -238,10 +344,30 @@ while running:
                 if heart_hitbox.colliderect(player_hitbox):
                     lives = min(lives + 1, 7)
                     active_powerup = None
+            elif active_powerup == 'armour':
+                armour_rect.x -= 5
+                armour_hitbox.center = armour_rect.center
+                screen.blit(armour_powerup_surf, armour_rect)
+                if armour_rect.right <= 0:
+                    active_powerup = None
+                if armour_hitbox.colliderect(player_hitbox):
+                    armour_active = True
+                    armour_timer = ARMOUR_DURATION
+                    active_powerup = None
+                    if is_night:
+                        player_frames = [player_walk_1_night_armour, player_walk_2_night_armour]
+                    else:
+                        player_frames = [player_walk_1_armour, player_walk_2_armour]
+
             if spring_active:
                 spring_timer_text = small_font.render(f"Jump Boost: {(spring_timer / 60):.2f} s", False, "White")
                 spring_timer_text_rect = spring_timer_text.get_rect(center=(650, 350))
                 screen.blit(spring_timer_text, spring_timer_text_rect)
+            
+            if armour_active:
+                armour_timer_text = small_font.render(f"Armour: {(armour_timer / 60):.2f} s", False, "Cyan")
+                screen.blit(armour_timer_text, armour_timer_text.get_rect(center=(620, 375)))
+                
 
 
             pygame.draw.rect(screen, "red", heart_hitbox, 2)
@@ -255,7 +381,7 @@ while running:
                     player_frame_index = (player_frame_index + 1) % len(player_frames)
                 player_surf = player_frames[player_frame_index]
             else:
-                player_surf = player_jump
+                player_surf = current_jump
                 animation_counter = 0 
 
         # Adjust player's vertical location then blit it
@@ -270,11 +396,25 @@ while running:
         # When player collides with enemy, game ends
         # When player collides with enemy, lose a life
             if egg_hitbox.colliderect(player_hitbox) and not is_invincible:
-                lives -= 1
-                is_invincible = True
-                invincible_timer = invincible_duration
-                if lives <= 0:
-                    is_playing = False
+                if armour_active:
+                    # Armour takes the hit: crack the egg, remove armour, go invincible briefly
+                    is_cracked = True
+                    armour_active = False
+                    armour_timer = 0
+                    is_invincible = True
+                    invincible_timer = invincible_duration
+                    if is_night:
+                        player_frames = [player_walk_1_night, player_walk_2_night]
+                    else:
+                        player_frames = [player_walk_1, player_walk_2]
+                else:
+                    lives -= 1
+                    is_invincible = True
+                    invincible_timer = invincible_duration
+                    if lives <= 0:
+                        is_playing = False
+                        is_entering_name = True
+                        username = ""
 
         # Count down invincibility
             if is_invincible:
@@ -303,14 +443,24 @@ while running:
                 instruct_surf = mini_font.render("<- -> to adjust sound_effects_vol", False, "white")
                 screen.blit(instruct_surf, instruct_surf.get_rect(center = (400,270)))
 
+                if is_night:
+                    night = "ON"
+                    night_color = "Green"
+                else:
+                    night = "OFF"
+                    night_color = "Red"
+                night_surf = small_font.render(f"Night Mode: {night}", False, night_color)
+                screen.blit(night_surf, night_surf.get_rect(center =(400, 310)))
+
+
                 back_surf = mini_font.render("ESC to go back to pause menu", False, "white")
-                screen.blit(back_surf, back_surf.get_rect(center=(200, 320)))
+                screen.blit(back_surf, back_surf.get_rect(center=(200, 380)))
             else:
                 pause_surf = game_font.render("PAUSED", False, "White")
                 resume_surf = small_font.render("ESC to Resume", False, "Gray")
                 quit_surf = small_font.render("Q to Quit", False, "Gray")
                 to_settings_surf = small_font.render("S to Settings", False, "Gray")
-
+                
                 screen.blit(pause_surf, pause_surf.get_rect(center=(400, 160)))
                 screen.blit(resume_surf, resume_surf.get_rect(center=(400, 215)))
                 screen.blit(quit_surf, quit_surf.get_rect(center=(400, 245)))
@@ -319,9 +469,19 @@ while running:
     # When game is over, display game over message
     else:
         screen.fill("black")
-        game_over_surf = game_font.render(f"Game Over! Final Score: {score}", False, "White")
-        game_over_rect = game_over_surf.get_rect(center=(400, 200))
-        screen.blit(game_over_surf, game_over_rect)
+        screen.blit(game_font.render(f"Game Over! Score: {score}", False, "White"),
+        game_font.render(f"Game Over! Score: {score}", False, "White").get_rect(center=(400, 80)))
+
+        if is_entering_name:
+            screen.blit(small_font.render("Enter your name:", False, "Gray"), small_font.render("Enter your name:", False, "Gray").get_rect(center=(400, 180)))
+            screen.blit(game_font.render(username + "|", False, "White"), game_font.render(username + "|", False, "White").get_rect(center=(400, 240)))
+            screen.blit(mini_font.render("Press ENTER to save", False, "Gray"), mini_font.render("Press ENTER to save", False, "Gray").get_rect(center=(400, 310)))
+        else:
+            scores = load_scores()
+            for i, (name, s) in enumerate(scores[:5]):
+                screen.blit(small_font.render(f"{i+1}. {name}  {s}", False, "White"),
+                        small_font.render(f"{i+1}. {name}  {s}", False, "White").get_rect(center=(400, 170 + i * 36)))
+            screen.blit(small_font.render("SPACE to play again", False, "Gray"), small_font.render("SPACE to play again", False, "Gray").get_rect(center=(400, 360)))
 
     # flip the display to put your work on screen
     pygame.display.flip()
